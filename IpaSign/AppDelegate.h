0c8adf8a7767118f984346cf9709dedecb97da40
guodong <>
2018-11-20 14:53:13 +0800

    test xcode sign ok

---
+++ IpaSign/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IpaSign
+//
+//  Created by guodong on 2018/11/20.
+//  Copyright © 2018年 Maizi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IpaSign/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IpaSign
+//
+//  Created by guodong on 2018/11/20.
+//  Copyright © 2018年 Maizi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
